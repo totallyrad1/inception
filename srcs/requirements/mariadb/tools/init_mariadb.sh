ca5e0c274d751665c228a66fb21eb0938ece6e57
Asaad Naji <asnaji@e2r7p10.1337.ma>
2024-10-29 22:15:50 +0100

    initial commit + worked a little bit on mariadb container

---
+++ srcs/requirements/mariadb/tools/init_mariadb.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/expect
+
+service mariadb start
+mysql_secure_installation
+
+expect "Enter current password for root (enter for none):"
+send "\n"
+
+expect "Switch to unix_socket authentication"
+send "Y\n"
+
+expect "Change the root password?"
+send "n\n"
+
+expect "Remove anonymous users?"
+send "Y\n"
+
+expect "Disallow root login remotely?"
+send "Y\n"
+
+expect "Remove test database and access to it?"
+send "Y\n"
+
+expect "Reload privilege tables now?"
+send "Y\n"
